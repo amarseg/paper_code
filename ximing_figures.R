d698213156bf1b5ee5758f683d05c4fcb511e0d4
amarseg <liamarseg@gmail.com>
2020-03-17 14:04:19 +0000

    made plots for SHREC complex

--- ximing_figures.R
+++ ximing_figures.R
@@ -8,7 +8,7 @@ library(ggrepel)
 isx_data <- read_csv('../data/isx_data.csv') %>%
   mutate(time = time - 1)
 
-int_genes <- c('taf2','taf3','taf5','taf11','taf12','spt7','wbp8','rpb1','rpb2','rpb9')
+int_genes <- c('clr1','clr2','clr3','mit1','ccq1','chp2')
 
 gene_ids <- read_tsv('ftp://ftp.pombase.org/pombe/names_and_identifiers/gene_IDs_names_products.tsv',
                      col_names = c('PomBaseID','second_id','name','chromosome','description',
@@ -22,15 +22,15 @@ int_omics <- omics[[2]] %>%
   left_join(isx_data, by = c('replicate' = 'rep', 'time_point.x' = 'time')) %>%
   filter(name %in% int_genes)
 
-ggplot(subset(int_omics, molecule == 'Protein'), aes(x = Length_Erode.M03..4., y = read_number.x, group = replicate, colour = time_point.x)) +
+ggplot(subset(int_omics, molecule == 'RNA'), aes(x = Length_Erode.M03..4., y = read_number.x, group = replicate, colour = time_point.x)) +
   facet_wrap(~name, scales = 'free') +
   geom_line() +
   geom_point() 
 
-ggsave('xi-ming_figure_read_number4.pdf')
+ggsave('SHREC_complex_reads.pdf')
 
-ggplot(subset(int_omics, molecule == 'Protein'), aes(x = Length_Erode.M03..4., y = log2foldchange, group = replicate, colour = replicate)) +
+ggplot(subset(int_omics, molecule == 'RNA'), aes(x = Length_Erode.M03..4., y = log2foldchange, group = replicate, colour = replicate)) +
   facet_wrap(~name, scales = 'free') +
   geom_line() +
   geom_point() 
-ggsave('xi-ming_figure_fold_change5.pdf')
+ggsave('SHREC_complex_fold_change.pdf')
